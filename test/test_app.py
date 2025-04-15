c73f29fe3472ec34a3dd4de0b21b52774398e802
e.vrublevskaya <e.vrublevskaya@visionlabs.ru>
2025-04-15 15:42:03 +0300

    Test_app updated

--- test/test_app.py
+++ test/test_app.py
@@ -8,12 +8,11 @@ from fastapi.testclient import TestClient
 from task_planner.configs.config import load_config
 from task_planner.main import app
 
-
-@pytest.fixture(scope="session", autouse=True)
-def set_test_config():
-    os.environ["CONFIG_FILE"] = "/home/helen/PycharmProjects/tasks_planner/test/test_config.yaml"
-    yield
-    del os.environ["CONFIG_FILE"]
+# @pytest.fixture(scope="session", autouse=True)
+# def set_test_config():
+#     os.environ["CONFIG_FILE"] = "/home/helen/PycharmProjects/tasks_planner/test/test_config.yaml"
+#     yield
+#     del os.environ["CONFIG_FILE"]
 
 
 @pytest.fixture(scope="session")
